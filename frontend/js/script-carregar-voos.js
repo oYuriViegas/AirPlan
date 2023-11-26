387bca8c94aa6b5520a49bd7e8ad0627e9f8b455
oYuriViegas <yuri.viegas9@gmail.com>
2023-11-26 12:56:02 -0300

    Revert "Mapa de assentos"
    This reverts commit acc5ddeb1e5161247dcf427e77e0d085b9b99eea.

--- frontend/js/script-carregar-voos.js
+++ frontend/js/script-carregar-voos.js
@@ -1,4 +1,4 @@
-// Importação das funções da API
+// Importações das funções da API (Certifique-se de que elas estão corretamente implementadas)
 import { getCidades, getVoos } from './api-voo.js';
 
 // Selecionar elementos do DOM
@@ -14,14 +14,65 @@ function carregarCidades() {
             const cidades = response.data;
             cidades.forEach(cidade => {
                 const optionOrigem = document.createElement('option');
-                const optionDestino = document.createElement('option');
                 optionOrigem.value = cidade.Nome;
-                optionDestino.value = cidade.Nome;
                 cidadeOrigemInput.appendChild(optionOrigem);
+
+                const optionDestino = document.createElement('option');
+                optionDestino.value = cidade.Nome;
                 cidadeDestinoInput.appendChild(optionDestino);
             });
         })
         .catch(error => console.error('Erro ao carregar cidades:', error));
+}
+
+// Carregar todos os voos inicialmente
+function carregarTodosVoos() {
+    getVoos()
+        .then(response => {
+            const voos = response.data;
+            atualizarTabelaVoos(voos);
+        })
+        .catch(error => console.error('Erro ao carregar voos:', error));
+}
+
+// Buscar voos com base na origem e destino
+function buscarVoos(origem, destino) {
+    getVoos()
+        .then(response => {
+            const voosFiltrados = response.data.filter(voo => 
+                voo.origem.includes(origem) && voo.destino.includes(destino)
+            );
+            atualizarTabelaVoos(voosFiltrados);
+        })
+        .catch(error => console.error('Erro ao buscar voos:', error));
+}
+
+// Atualizar a tabela de voos
+function atualizarTabelaVoos(voos) {
+    voosTableBody.innerHTML = '';
+    voos.forEach(voo => {
+        const row = createTableRow(voo);
+        voosTableBody.appendChild(row);
+    });
+}
+
+// Função para criar linhas da tabela com os dados dos voos
+function createTableRow(voo) {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>${voo.origem}</td>
+        <td>${voo.destino}</td>
+        <td>${formatarDataHora(voo.partida)}</td>
+        <td>${formatarDataHora(voo.chegada)}</td>
+        <td>${voo.preco}</td>
+    `;
+    return row;
+}
+
+// Função para formatar data e hora
+function formatarDataHora(dataHora) {
+    const data = new Date(dataHora);
+    return data.toLocaleString(); // Converte para formato local de data e hora
 }
 
 // Evento de envio do formulário de busca de voos
@@ -32,36 +83,8 @@ formBuscaVoo.addEventListener('submit', function(event) {
     buscarVoos(origem, destino);
 });
 
-// Buscar voos com base na origem e destino
-function buscarVoos(origem, destino) {
-    getVoos()
-        .then(response => {
-            const voos = response.data.filter(voo => 
-                voo.origem === origem && voo.destino === destino
-            );
-            atualizarTabelaVoos(voos);
-        })
-        .catch(error => console.error('Erro ao buscar voos:', error));
-}
-
-// Atualizar a tabela de voos
-function atualizarTabelaVoos(voos) {
-    voosTableBody.innerHTML = '';
-    voos.forEach(voo => {
-        const row = document.createElement('tr');
-        row.innerHTML = `
-            <td>${voo.vooId}</td>
-            <td>${voo.origem}</td>
-            <td>${voo.destino}</td>
-            <td>${voo.dataPartida}</td>
-            <td>${voo.dataChegada}</td>
-            <td>${voo.preco}</td>
-        `;
-        voosTableBody.appendChild(row);
-    });
-}
-
 // Inicializar a página
 document.addEventListener('DOMContentLoaded', function() {
     carregarCidades();
+    carregarTodosVoos();
 });
